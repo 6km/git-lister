7eaec075b033d7d43c370ec9b07e3db494d602a4
Mohammed <gsft22221@gmail.com>
2022-07-19 23:57:11 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -8,6 +8,16 @@ const apiRoutes = require("./api");
 
 app.use("/api", apiRoutes);
 
+if (
+    process.env.LD_LIBRARY_PATH == null ||
+    !process.env.LD_LIBRARY_PATH.includes(
+        `${process.env.PWD}/node_modules/canvas/build/Release:`,
+    )
+) {
+    process.env.LD_LIBRARY_PATH = `${process.env.PWD
+        }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`🚀 Server is running in port ${PORT}`));
