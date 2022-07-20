2779a02967f92042588f6447b1a32725566bc380
Mohammed <gsft22221@gmail.com>
2022-07-20 12:04:47 +0200

    update stars.js

--- src/api/stars.js
+++ src/api/stars.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router()
 
-const { drawUsers, getStargazers, drawUsersSVG } = require("../utils");
+const { getStargazers, drawUsersSVG } = require("../utils");
 const { cache } = require("../middlewares")
 
 const axios = require("axios").default
