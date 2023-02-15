69ea378456c8c0c4e4e589c2a8892fb2bd465a4c
Mohammed <gsft22221@gmail.com>
2023-02-15 02:34:49 +0200

    Reduce cache duration to 60 seconds

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DEFAULT_CACHE_TIME: 300, // 300 seconds (5 minutes)
+    DEFAULT_CACHE_TIME: 60, // 60 seconds (1 minute)
     STARGAZERS: {
         MAX_USERS_PER_IMAGE: 15,
         DEFAULT_MORE_BUTTON_TEXT: "… and more",
