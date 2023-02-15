4e2990d246077511d1bdb3fa3c04eb4de6e32b5d
Mohammed <gsft22221@gmail.com>
2023-02-15 03:08:29 +0200

    Change cache time to 180 seconds

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DEFAULT_CACHE_TIME: 60, // 60 seconds (1 minute)
+    DEFAULT_CACHE_TIME: 180, // 180 seconds (3 minute)
     STARGAZERS: {
         MAX_USERS_PER_IMAGE: 15,
         DEFAULT_MORE_BUTTON_TEXT: "… and more",
